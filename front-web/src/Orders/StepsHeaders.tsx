aa4a0fc895afea8f30d3edb599e108e32b2448b2
Robertoromg <robertoro_mg@outlook.com>
2021-01-09 21:26:17 -0300

    Adicionando map React Leaflet

--- front-web/src/Orders/StepsHeaders.tsx
+++ front-web/src/Orders/StepsHeaders.tsx
@@ -1,7 +1,7 @@
 function StepsHeaders(){
 
     return(
-        <header className="ordeers-steps-container">
+        <header className="orders-steps-container">
             <div className="orders-steps-content">
                 <h1 className="steps-title">
                     SIGA AS <br/> 
--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from 'react';
+import { fetchProducts } from '../Api';
+import OrderLocation from './OrderLocation';
+import ProductsList from './ProductsList';
 import StepsHeaders from './StepsHeaders';
 import './styles.css';
+import { Product } from './types';
 
 function Orders(){
+
+ const [products, setProducts] = useState<Product[]>([]);
+console.log(products)
+useEffect(()=>{
+    
+    fetchProducts()
+    .then(response =>setProducts(response.data))
+    .catch(error=> console.log(error))
+
+    }, []);
 
     return(
         <div className="orders-container">
            <StepsHeaders/>
+           <ProductsList products={products}/>
+           <OrderLocation/>
         </div>
         
     )
